d50657ad5b20e093488748d7e46cfa688e42b81c
Nathnael Abera <Nathnael.Abera@CCI.CORP>
2024-03-19 12:16:55 +0300

    change base url

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://89.116.51.11:80", credentials: true }));
 
 app.use("/api/user", user);
 app.use("/api/product", product);
